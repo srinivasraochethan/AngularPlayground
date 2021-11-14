faaf31a309f43135918fa93301e35febc2e05465
Chethan Srinivas Rao <chethan.srinivas_rao@siemens-healthineers.com>
2021-11-14 22:33:39 +0530

    Folder Structure Change

--- Angular13Project/src/app/app.component.spec.ts
+++ Angular13Project/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularUpgrade'`, () => {
+  it(`should have as title 'Angular13Project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularUpgrade');
+    expect(app.title).toEqual('Angular13Project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularUpgrade app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular13Project app is running!');
   });
 });
--- Angular13Project/src/app/app.component.ts
+++ Angular13Project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularUpgrade';
+  title = 'Angular13Project';
 }
